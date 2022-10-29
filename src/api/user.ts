398b1f04549ebb19d4fbe446744a01bc6f9afbab
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-10-29 19:11:25 +0330

    Update dummy data

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,18 @@
 export const getUserContactChannels = () => {
   return [
     { id: "10", name: "twitter", link: "https://fa.com" },
-    { id: "11", name: "facebook", link: "https://x.com" },
+    { id: "11", name: "facebook", link: "https://test.com" },
     { id: "12", name: "instagram", link: "https://instagram.com" },
+    { id: "13", name: "website", link: "https://websike.com" },
+    { id: "14", name: "linkedin", link: "https://123.com" },
+    { id: "15", name: "facebook", link: "https://abc.com" },
+    { id: "16", name: "website", link: "https://selisenfslkenslefn.com" },
+    { id: "17", name: "twitter", link: "https://fa.com" },
+    { id: "18", name: "facebook", link: "https://test.com" },
+    { id: "19", name: "instagram", link: "https://instagram.com" },
+    { id: "20", name: "website", link: "https://websike.com" },
+    { id: "21", name: "linkedin", link: "https://123.com" },
+    { id: "22", name: "facebook", link: "https://abc.com" },
+    { id: "23", name: "website", link: "https://selisenfslkenslefn.com" },
   ];
 };
--- src/features/ContactChannels/api/index.ts
+++ src/features/ContactChannels/api/index.ts
@@ -1,13 +1,4 @@
 export const getContactChannelsOptions = () => {
-  // return [
-  //   { id: 1, name: "twitter" },
-  //   { id: 2, name: "instagram" },
-  //   { id: 3, name: "facebook" },
-  //   { id: 4, name: "telegram" },
-  //   { id: 5, name: "linkedin" },
-  //   { id: 6, name: "website" },
-  // ];
-
   return [
     "twitter",
     "instagram",
